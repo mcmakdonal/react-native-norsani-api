8db3cbb0fb810796688770425fcc0a0801e4f4c9
Mahmud HAmid <www770@gmail.com>
2019-03-11 14:44:40 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,20 +1,22 @@
-var WooCommerceAPI = require('react-native-woocommerce-api');
+import NorsaniAPI from 'react-native-norsani-api';
 
-var WooCommerceAPI = new WooCommerceAPI({
-  url: 'https://yourstore.com', // Your store URL
-  ssl: true,
-  consumerKey: 'ck_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Your consumer secret
-  consumerSecret: 'cs_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Your consumer secret
+const Norsani = new NorsaniAPI({
+  url: 'https://yourstore.com', // Your Norsani Website URL
+  isSsl: true,
+  verifySsl: true,
+  consumerKey: 'ck_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Your WooCommerce consumer secret
+  consumerSecret: 'cs_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Your WooCommerce consumer secret
   wpAPI: true, // Enable the WP REST API integration
-  version: 'wc/v2', // WooCommerce WP REST API version
+  NorsaniVersion: 'v1', // Norsani REST API version
+  WCVersion: 'v3', // WooCommerce REST API version
   queryStringAuth: true
 });
 
- 	WooCommerceAPI.get('products', {
-        })
-          .then(data => {
-          	console.log(data);
-          })
-          .catch(error => {
-          	console.log(error)
-          })
+Norsani.get('vendors', {
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error)
+  });
